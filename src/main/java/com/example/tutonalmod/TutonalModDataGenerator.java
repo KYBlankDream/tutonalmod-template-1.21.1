c646c0f67be74feba1c5ed64887de60d0bdf2d26
KY_BlankDream <1838186480@qq.com>
2025-08-02 10:36:57 +0800

    1

---
+++ src/main/java/com/example/tutonalmod/TutonalModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.example.tutonalmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutonalModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
